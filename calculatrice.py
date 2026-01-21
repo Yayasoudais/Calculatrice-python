2816ec36337d13e62cddf517ddf71650d07a4a97
Mamadou Yaya Diallo <mamadouyaya.soudais34@gmail.com>
2026-01-22 00:52:48 +0100

    Ajoute multiplication et division

--- calculatrice.py
+++ calculatrice.py
@@ -1,6 +1,14 @@
 def addition(a,b):
     return a+b
 
+def multuplication(a,b):
+    return a*b
+
+def division(a,b):
+    if b== 0:
+        return "Erreu: division par zéro"
+    return a/b
+
 
 if __name__== "__main__":
     print("Addition : 5+3 = ",addition(5,3))
